8bcb2840ade6e4068292cd1a13cf614856e3cccc
Fabricio Taveira Carvalho Marques <tr26330es@trf1.gov.br>
2023-10-18 14:03:34 -0300

    Fifth Commit

--- js/employer.js
+++ js/employer.js
@@ -3,15 +3,26 @@ const btnElegibility = document.getElementById("btnElegibility");
 const btnSignIn = document.getElementById("btnSignIn");
 const btnBack = document.getElementById("btnBack");
 const btnHamburguer = document.getElementById("btnHamburguer");
-const btnSettings = document.getElementById("btnBack");
+const btnSettings = document.getElementById("btnSettings");
 const menuEmployer = document.getElementById("menuEmployer");
-
+const menuSettings = document.getElementById("menuSettings");
 
 btnHamburguer.addEventListener('click',()=>{
     if(menuEmployer.style.display === "none" || menuEmployer.style.display === ""){
         menuEmployer.style.display = "flex";
+        menuSettings.style.display ="none";
+
     } else {
         menuEmployer.style.display ="none";
+    }
+    
+})
+btnSettings.addEventListener('click',()=>{
+    if(menuSettings.style.display === "none" || menuSettings.style.display === ""){
+        menuSettings.style.display = "flex";
+        menuEmployer.style.display ="none";
+    } else {
+        menuSettings.style.display ="none";
     }
     
 })
